7f205824c54c7ce1b5c191da6fae8e358a9146af
riccardoscheda <riccardo.scheda@gmail.com>
2020-12-05 17:45:46 +0100

    update ,need to move functions for formatting data

--- personal/RS/pipeline/train.py
+++ personal/RS/pipeline/train.py
@@ -6,11 +6,94 @@ from argparse import ArgumentParser
 import os
 import logging
 from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+# Keep only columns of interest
+id_cols = ['CountryName',
+           'RegionName',
+           'GeoID',
+           'Date']
+cases_col = ['NewCases']
+npi_cols = ['C1_School closing',
+            'C2_Workplace closing',
+            'C3_Cancel public events',
+            'C4_Restrictions on gatherings',
+            'C5_Close public transport',
+            'C6_Stay at home requirements',
+            'C7_Restrictions on internal movement',
+            'C8_International travel controls',
+            'H1_Public information campaigns',
+            'H2_Testing policy',
+            'H3_Contact tracing',
+            'H6_Facial Coverings']
 
+# Helpful function to compute mae
+def mae(pred, true):
+    return np.mean(np.abs(pred - true))
+
+#This Function need to be outside the training process
 def create_dataset(df):
+    """
+    From XPRIZE jupyter, this function merges country and region, fills any missing cases
+    and fills any missing pis
+    """
     # Add RegionID column that combines CountryName and RegionName for easier manipulation of data
     df['GeoID'] = df['CountryName'] + '__' + df['RegionName'].astype(str)
     # Add new cases column
+    # Add new cases column
+    df['NewCases'] = df.groupby('GeoID').ConfirmedCases.diff().fillna(0)
+
+
+    # Fill any missing case values by interpolation and setting NaNs to 0
+    df.update(df.groupby('GeoID').NewCases.apply(
+        lambda group: group.interpolate()).fillna(0))
+
+    # Fill any missing NPIs by assuming they are the same as previous day
+    for npi_col in npi_cols:
+        df.update(df.groupby('GeoID')[npi_col].ffill().fillna(0))
+
+
+    return df
+
+def skl_format(df, lookback_days=30):
+    """
+    Takes data and makes a formatting for sklearn
+    """
+    # Create training data across all countries for predicting one day ahead
+    X_cols = cases_col + npi_cols
+    y_col = cases_col
+    X_samples = []
+    y_samples = []
+    geo_ids = df.GeoID.unique()
+    for g in geo_ids:
+        gdf = df[df.GeoID == g]
+        all_case_data = np.array(gdf[cases_col])
+        all_npi_data = np.array(gdf[npi_cols])
+
+        # Create one sample for each day where we have enough data
+        # Each sample consists of cases and npis for previous nb_lookback_days
+        nb_total_days = len(gdf)
+        for d in range(lookback_days, nb_total_days - 1):
+            X_cases = all_case_data[d-lookback_days:d]
+
+            # Take negative of npis to support positive
+            # weight constraint in Lasso.
+            X_npis = -all_npi_data[d - lookback_days:d]
+
+            # Flatten all input data so it fits Lasso input format.
+            X_sample = np.concatenate([X_cases.flatten(),
+                                       X_npis.flatten()])
+            y_sample = all_case_data[d + 1]
+            X_samples.append(X_sample)
+            y_samples.append(y_sample)
+
+    X_samples = np.array(X_samples)
+    y_samples = np.array(y_samples).flatten()
+
+
+
+    return X_samples, y_samples
 
 
 
@@ -42,7 +125,19 @@ if __name__ == '__main__':
 
     model = eval(config_data["model"])
 
-    model.train()
+    cols = config_data["CountryName"]
+    new_df = pd.DataFrame()
+    for col in cols:
+        new_df = new_df.append(df[df["CountryName"] == col])
+    X_samples, y_samples = skl_format(create_dataset(new_df))
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X_samples,
+                                                        y_samples,
+                                                        test_size=0.2,
+                                                        random_state=301)
+
+    model.fit(X_train, y_train)
+    print(new_df)
     print("Elapsed time:", time() - start)
     logging.info("Elapsed time:" + str(time() - start))
 
