6f67a6168b7c249219b59f05e9713321099b0f6e
Mat092 <mattia.ceccarelli3@gmail.com>
2020-12-15 15:10:07 +0100

    start Scaler for data

--- personal/MC/pipeline/train.py
+++ personal/MC/pipeline/train.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -10,14 +10,15 @@ from time import time
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
-from utils import mae, create_dataset, skl_format, add_temp
+from utils import mae, create_dataset, skl_format
+from utils import add_temp, add_population_data, add_HDI
 
-# Keep only columns of interest
 id_cols = ['CountryName',
            'RegionName',
            'GeoID',
@@ -42,6 +43,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, filename='logfile', filemode='a+',
                         format='%(asctime)-15s %(levelname)-8s %(message)s')
     logging.info('################### TRAINING ##################')
+    # logging.captureWarnings(True)
 
     # Reads info from configuration file
     parser = argparse.ArgumentParser()
@@ -49,7 +51,7 @@ if __name__ == '__main__':
                         dest='JSONfilename',
                         help='JSON configuration file',
                         metavar='FILE',
-                        default='train_config.json')
+                        default='config.json')
     args = parser.parse_args()
 
     print('Loading', args.JSONfilename, '...')
@@ -57,87 +59,117 @@ if __name__ == '__main__':
     with open(args.JSONfilename) as f:
         config_data = json.load(f)
 
-    start = time()
+    # Loading config parameters
+    lookback_days = config_data['lookback_days']
+    train_config = config_data['train']
+
+    input_dataset = train_config['input_file']
+    models_output_dir = train_config['models_output_dir']
+
+    start_date = train_config['start_date']
+    end_date = train_config['end_date']
+
+    moving_average = eval(train_config['moving_average'])  # it's a string in json, we want bool
+    models = train_config['models']
+    countries = train_config['countries']
+
+    scale = config_data['scale']
+
+    # Additional Columns adder
+    adj_cols_fixed = config_data['adj_cols_fixed']
+    adj_cols_time = config_data['adj_cols_time']
 
     # Reading file with historical interventions
-    df = pd.read_csv(config_data['input_file'],
+    start = time()
+    df = pd.read_csv(input_dataset,
                      parse_dates=['Date'],
                      encoding='ISO-8859-1',
                      dtype={'RegionName': str,
                             'RegionCode': str},
                      error_bad_lines=True)
 
-    # Adding temperatures
-    # df = add_temp(df)
+    # Selecting choosen time period from config file
+    df = df[(df.Date > start_date) & (df.Date < end_date)]
+    df = create_dataset(df)
 
-    # Reading the choosen model
-    models = config_data['models']
+    if scale:
+
+      if scale == 1:
+        scaler = MinMaxScaler(feature_range=(0, 1), copy=True)
+
+      elif scale == 2:
+        scaler == StandardScaler(copy=True)
+
+      else:
+        raise ValueError('Value {} not recognized for variable scaler'.format(scaler))
+
+      df = scaler.fit_transform(df)
+
+    # Selecting countries of interest from config file
+    # TO TEST ALL COUNTRY, WRITE "countries" : "" in jsonfile
+    if countries:
+        cols = countries
+    else:
+        cols = list(df.CountryName.unique())
+
+    new_df = pd.DataFrame()
+
+    for col in cols:
+        new_df = new_df.append(df[df.CountryName == col])
+
+    # formatting data for scikitlearn
+    X_samples, y_samples = skl_format(new_df,
+                                      moving_average,
+                                      lookback_days=lookback_days,
+                                      adj_cols_fixed=adj_cols_fixed,
+                                      adj_cols_time=adj_cols_time,
+                                      )
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X_samples,
+                                                        y_samples,
+                                                        test_size=0.2,
+                                                        random_state=301)
 
     # Start looping on models keys, every model cointains: name and param_grid
     for model_name in models.keys():
-      model = eval(model_name)
-      param_grid = models[model_name]
 
-      for param in models[model_name]:
-        param_grid[param] = eval(param_grid[param])
+        model = eval(model_name)
+        param_grid = models[model_name]
 
-      gcv = GridSearchCV(estimator=model,
-                         param_grid=param_grid,
-                         scoring=None,  # TODO
-                         n_jobs=2,      # -1 is ALL PROCESSOR AVAILABLE
-                         cv=None,       # None is K=5 fold CV
-                         refit=True,
-                         )
+        for param in models[model_name]:
+          param_grid[param] = eval(param_grid[param])
 
-      # Selecting countries of interest from config file
-      if config_data['countries'] != '':
-        cols = config_data['countries']
+        gcv = GridSearchCV(estimator=model,
+                           param_grid=param_grid,
+                           scoring=None,  # TODO
+                           n_jobs=1,      # -1 is ALL PROCESSOR AVAILABLE
+                           cv=2,          # None is K=5 fold CV
+                           refit=True,
+                           )
 
-      else:
-        cols = list(df['CountryName'].unique())
+        # Fit the GridSearch
+        gcv.fit(X_samples, y_samples)
 
-      new_df = pd.DataFrame()
-      for col in cols:
-        new_df = new_df.append(df[df['CountryName'] == col])
+        # Evaluate model
+        train_preds = gcv.predict(X_train)
+        train_preds = np.maximum(train_preds, 0)  # Don't predict negative cases
+        print('\nTrain MAE:', mae(train_preds, y_train))
 
-      # formatting data for scikitlearn
-      lookback_days = config_data['lookback_days']
-      X_samples, y_samples = skl_format(create_dataset(new_df), lookback_days)
+        # test_preds = model.predict(X_test)
+        # test_preds = np.maximum(test_preds, 0) # Don't predict negative cases
+        # print('Test MAE:', mae(test_preds, y_test))
 
-      # Split data into train and test sets
-      X_train, X_test, y_train, y_test = train_test_split(X_samples,
-                                                          y_samples,
-                                                          test_size=0.2,
-                                                          random_state=42)
+        model_path = os.path.join(models_output_dir, model_name[:-2] + '.pkl')
 
-      # Fit the GridSearchCV
-      gcv.fit(X_train, y_train)
+        print('Saving model in ', model_path)
+        logging.info('Saving model in ' + str(model_path))
 
-      # Evaluate model
-      train_preds = gcv.predict(X_train)
-      train_preds = np.maximum(train_preds, 0)  # Don't predict negative cases
-      print('Train MAE:', mae(train_preds, y_train))
+        # Save model to file
+        if not os.path.exists(models_output_dir):
+            os.mkdir(models_output_dir)
 
-      # test_preds = model.predict(X_test)
-      # test_preds = np.maximum(test_preds, 0) # Don't predict negative cases
-      # print('Test MAE:', mae(test_preds, y_test))
+        with open(model_path, 'wb') as model_file:
+            pickle.dump(gcv, model_file)
 
-      print('Saving model in models/' + model_name[:-2] + '.pkl')
-      logging.info('Saving model in models/' + model_name[:-2] + '.pkl')
-
-      # Save model to file
-      if not os.path.exists('models'):
-        os.mkdir('models')
-
-      with open('models/' + model_name[:-2], 'wb') as model_file:
-        pickle.dump(gcv, model_file)
-
-      print('Elapsed time:', time() - start)
-      logging.info('Elapsed time:' + str(time() - start))
-
-    # mode
-#    mode = 0o666
-    # path = os.path.join(parent_dir, directory)
-    # output_path='data/'
-    # if not os.path.exists(output_path):
-    #     os.mkdir(output_pathd)
+        print('Elapsed time: {:.5} s'.format(time() - start))
+        logging.info('Elapsed time: ' + str(time() - start))
